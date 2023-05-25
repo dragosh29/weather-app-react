9d0bb182f552b0230e78e0bf577beb401614265c
dragosh29 <alexandru.dragos96@gmail.com>
2023-05-25 16:06:05 +0300

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getPosition = () =>
+                new Promise((resolve, reject) =>
+                    navigator.geolocation.getCurrentPosition(resolve, reject)
+                );
+
+            try {
+                const position = await getPosition();
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                setLat(latitude);
+                setLong(longitude);
+
+                const response = await fetch(`${process.env.REACT_APP_API_URL}?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`);
+                const result = await response.json();
+                setData(result);
+                console.log(result);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+            {typeof data.main !== 'undefined' ? (
+                <Weather weatherData={data} />
+            ) : (
+                <div>
+                    <Dimmer active>
+                        <Loader>Loading..</Loader>
+                    </Dimmer>
+                </div>
+            )}
+        </div>
+    );
 }
-
-export default App;
